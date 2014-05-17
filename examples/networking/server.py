438ee7560df1c6fef6b3746e55b0e5c57fec04b4
Matt Kipps <matkips@gmail.com>
2014-05-16 22:11:08 -0700

    simple server

--- examples/networking/server.py
+++ examples/networking/server.py
@@ -1,7 +1,11 @@
 #!/usr/bin/python2.7
 # the game server
 
+# python libs
 import socket
+
+# our libs
+import message
 
 HOST = ''
 PORT = 7307
@@ -11,9 +15,20 @@ server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 server.bind((HOST,PORT))
 server.listen(LISTEN_QUEUE)
 
-conn, addr = server.accept()
-print "connected with", addr
+client, addr = server.accept()
+print "-- connected with {} --".format(addr[0])
+print "-- client's port is {} --".format(addr[1])
 
-conn.close()
+msg = message.recv_message(client)
+
+print "client says '{}'".format(msg)
+
+msg = message.recv_message(client)
+
+if msg == "list":
+    print "client wants list of users"
+else:
+    print "unknown command"
 
 
+client.close()
