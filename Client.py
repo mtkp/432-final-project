aee6eb4eb24a94ea79beddcd6cef6072e675c20c
Greg Parker <gparker24@gmail.com>
2014-05-16 21:15:47 -0700

    now able to send length + username

--- Client.py
+++ Client.py
@@ -1,13 +1,27 @@
 #!/usr/bin/python2.7
 
 import socket
-#Client
+#Client    
 
 #create an INET, STREAMing socket
 s = socket.socket( socket.AF_INET, socket.SOCK_STREAM )
 
 #now connect to the web server on port 7307
 s.connect(("uw1-320-15.uwb.edu", 7307))
+h
+name = "Greg"
+len_name = "{0:04d}".format(len(name))
+
+length = len(name)
+total_len = length + len(len_name)
+msg = len_name + name
+totalsent = 0
+
+while totalsent < total_len:
+    sent = s.send(msg[totalsent:])
+    if sent == 0:
+        raise RuntimeError("socket connection broken")
+    totalsent = totalsent + sent
 
 s.close()
 
