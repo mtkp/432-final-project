56a03cd6b7a0ccfe16bcd7aa9d667b1e4f4db7bc
Matt Kipps <matkips@gmail.com>
2014-05-31 21:37:31 -0700

    rename message as messenger

--- app/clienttestdriver.py
+++ app/clienttestdriver.py
@@ -5,28 +5,7 @@ import time
 import gameclient
 
 client = gameclient.GameClient()
-# make gameobject register send message
 
-#!/usr/bin/python2.7
-# client
-
-# our libs
-import gameclient
-
-
-def print_users(user_list):
-    print '\n'.join(" - {}".format(user) for user in user_list)
-
-def print_games(game_list):
-    print '\n'.join(
-        "'{}' [{}] ({}/{})".format(game[0], game[1], len(game[2]), game[3])
-        for game in game_list)
-
-def print_game(game):
-    print "####"
-    print "'{}' [{}] ({}/{})".format(game[0], game[1], len(game[2]), game[3])
-    print_users(game[2])
-    print "####"
 
 # get server name from user
 
@@ -34,7 +13,7 @@ client = gameclient.GameClient()
 
 while True:
     username = raw_input("username: ")
-    server = "10.0.1.14" # raw_input("server: ")
+    server = raw_input("server: ")
 
     try:
         client.register(username, server)
@@ -48,13 +27,12 @@ while True:
 
 
 while True:
-    msg = client.get_messages()
-    
-    
-    if msg:
-        print str(msg[0])
+    client.update()
 
-    time.sleep(0.04)
+    if client.has_messages():
+        print "{} -- {}".format(*client.get_message())
 
-    client._send(("test", "test"))
-   
+    time.sleep(0.1)
+
+    # client._send(("test", "test"))
+
