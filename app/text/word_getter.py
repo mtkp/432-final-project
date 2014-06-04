285773473787d8518a9e59ca1f846e6e82cfbcda
Matt Kipps <matkips@gmail.com>
2014-06-03 22:10:37 -0700

    clean up

--- app/text/word_getter.py
+++ app/text/word_getter.py
@@ -2,19 +2,22 @@
 
 import random
 
-WORDS_FILE = "min_8"
+WORDS_FILE = "/usr/share/dict/words"
 
 # returns list of ten words chosen randomly form larger list
 def get_random_words(all_words):
     real_word_list = []
     range_max = len(all_words) - 1
-    for _ in xrange(500):
+    for _ in xrange(5000):
         real_word_list.append(all_words[random.randint(0, range_max)])
     return real_word_list
 
 # opens text file and rturns a small list of the words
 def read_words(words_file=WORDS_FILE):
-    all_words = [word for line in open(words_file, 'r') for word in line.split()]
+    all_words = [
+        word for line in open(words_file, 'r')
+             for word in line.split() if len(word) > 7
+        ]
     return get_random_words(all_words)
 
 
